0cbb9c315c88960308d6d3e4df5a3aa8952b4c3b
Scott Field <sfield13@ivytech.edu>
2023-05-02 21:05:05 -0700

    changed request file path

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,11 @@ from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'templates/blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'templates/blog/post_detail.html', {'post': post})
 
 # Create your views here.
 def post_new(request):
@@ -24,4 +24,4 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'templates/blog/post_edit.html', {'form': form})
